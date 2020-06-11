bd3f952089c78d59a5d7b495900b1e0674a60076
Andres Cortes <1805@holbertonschool.com>
2020-06-11 05:10:42 +0000

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+#include<stdio.h>
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
