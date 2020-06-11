65e45fd9bc8f3c71d47295197ec755f67e9c6b5a
Andres Cortes <1805@holbertonschool.com>
2020-06-11 22:39:53 +0000

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar (x);
+		x++;
+	}
+
+	return (0);
+}
