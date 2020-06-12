bcb15048753415254baec28933d8b08a1be1b882
Andres Cortes <1805@holbertonschool.com>
2020-06-12 04:05:41 +0000

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	int zero;
+
+	for (zero = 0; zero <= 9; zero++)
+	{
+		printf("%d", zero);
+	}
+	printf("\n");
+
+	return (0);
+}
