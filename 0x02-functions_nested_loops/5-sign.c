acb389d60f430ca661d92b3b5e6001d4adc661c9
Andres Cortes <1805@holbertonschool.com>
2020-06-16 20:54:30 +0000

    5sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+int print_sign(int n);
+/**
+ *print_sign- a function to print the sign of a number
+ *@n: +, 0 or - number
+ *Return: Always 0
+ */
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if(n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
