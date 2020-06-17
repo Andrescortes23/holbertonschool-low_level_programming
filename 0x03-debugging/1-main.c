93b82776cd776781cdbfd739938823e4f3e3eb4b
Andres Cortes <1805@holbertonschool.com>
2020-06-17 03:30:22 +0000

    1main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,13 @@ int main(void)
         printf("Infinite loop incoming :(\n");
 
         i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 *putchar(i);
+	 *}
+	 */
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
-	printf("You need to add i++ to i become gigher than 10 and the loop ends\n");
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
