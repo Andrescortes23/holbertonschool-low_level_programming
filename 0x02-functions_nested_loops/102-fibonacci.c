2c0205a944acc0291aa3bebd553e7c54fb8cb6fd
Andres Cortes <1805@holbertonschool.com>
2020-06-18 03:38:55 +0000

    fibona

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - to 50
+ *Return: 0
+ */
+int main(void)
+{
+	long int f1, f2, temp;
+	int i = 0;
+
+	f2 = 1;
+	temp = 0;
+	f1 = 0;
+	while (i < 50)
+	{
+		temp = f2;
+		f2 = f1 + f2;
+		f1 = temp;
+		printf("%ld", f2);
+		i++;
+		if (i <= 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
