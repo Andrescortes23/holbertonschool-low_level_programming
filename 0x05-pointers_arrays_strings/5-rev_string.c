1e5737587380025fe67d9a39fb1209fc9c522bd6
Andres Cortes <1805@holbertonschool.com>
2020-06-24 22:31:06 +0000

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,23 @@
  */
 void rev_string(char *s)
 {
-	*s = 'n';
-	*(s + 1) = 'o';
-	*(s + 2) = 't';
-	*(s + 3) = 'r';
-	*(s + 4) = 'e';
-	*(s + 5) = 'b';
-	*(s + 6) = 'l';
-	*(s + 7) = 'o';
-	*(s + 8) = 'H';
+	int doil;
+	int doli;
+	char lodi;
+
+	doil = 0;
+	while (s[doil] != '\0')
+	{
+		doil++;
+	}
+	doli = 0;
+	while (doil > doli)
+	{
+		lodi = s[doil - 1];
+		s[doil - 1] = s[doli];
+		s[doli] = lodi;
+		doli++;
+		doil--;
+	}
+
 }
