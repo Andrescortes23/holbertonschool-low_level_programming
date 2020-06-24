54fbf024a36cc5ce0675f098bb88afa11f10ec40
Andres Cortes <1805@holbertonschool.com>
2020-06-24 03:17:30 +0000

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *puts_half - half string
+ *@str: string
+ *Return: 0
+ */
+void puts_half(char *str)
+{
+
+	int den = 0;
+	int mitad = 0;
+
+	while (str[den] != '\0')
+	{
+		den++;
+	}
+
+	mitad = (den) / 2;
+
+	while (str[mitad] != '\0')
+	{
+		_putchar (str[mitad]);
+		mitad++;
+	}
+
+
+	_putchar ('\n');
+}
