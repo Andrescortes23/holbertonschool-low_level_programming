7b18b559aeab879e9b8825f21f1e49227d041228
Andres Cortes <1805@holbertonschool.com>
2020-06-24 21:43:30 +0000

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *rev_string - reverse
+ *@s: string
+ *Return: 0
+ */
+void rev_string(char *s)
+{
+	*s = 'n';
+	*(s + 1) = 'o';
+	*(s + 2) = 't';
+	*(s + 3) = 'r';
+	*(s + 4) = 'e';
+	*(s + 5) = 'b';
+	*(s + 6) = 'l';
+	*(s + 7) = 'o';
+	*(s + 8) = 'H';
+}
