e35ef9eec84bfa0e53bf829db4eaea10ef025282
Andres Cortes <1805@holbertonschool.com>
2020-06-26 00:55:30 +0000

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+	int b;
+	int oxy;
+
+	for (a = 0; *(s1 + a) != '\0'; a++)
+	{
+	}
+	for (b = 0; *(s2 + b) != '\0'; b++)
+	{
+	}
+
+	while (*(s1 + a) != '\0')
+	{
+		if (*(s2 + b) > *(s1 + a))
+			oxy = -15;
+		if (*(s2 + b) < *(s1 + a))
+			oxy = 15;
+		if (*(s1 + a) == '\0')
+			oxy = 0;
+	}
+	return oxy;
+}
