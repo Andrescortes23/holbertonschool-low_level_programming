0dc799e45c8d26eaad3fc4d2cce632d909a6d292
Andres Cortes <1805@holbertonschool.com>
2020-06-26 04:15:22 +0000

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *leet - to 1337
+ *@s: pointer
+ *Return: s
+ */
+char *leet(char *s)
+{
+	char *let = "aAeEoOtTlL";
+	char *num = "4433007711";
+	int a;
+	int b;
+
+	for (a = 0; *(s + a); a++)
+	{
+		for (b = 0; *(let + b); b++)
+		{
+			if (*(s + a) == *(let + b))
+				{
+					*(s + a) = *(num + b);
+				}
+		}
+	}
+	return (s);
+}
