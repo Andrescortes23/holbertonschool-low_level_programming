f0dd6a79cc0d5e0e8a7463c88bc7b228ffa9990f
Andres Cortes <1805@holbertonschool.com>
2020-06-26 04:47:17 +0000

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *cap_string - to capit
+ *@s: pointer
+ *Return: s
+ */
+char *cap_string(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	for(;s[a] >= 97 && s[a] <= 122; a++)
+	{
+	}
+	if (s[a - 1] == ',' || s[a - 1] == ';' || s[a - 1] == '.' ||
+	    s[a - 1] == '!' || s[a - 1] == '?' || s[a - 1] == '"' ||
+	    s[a - 1] == '(' || s[a - 1] == ')';
+
+	    return (s)
+	    }
+}
