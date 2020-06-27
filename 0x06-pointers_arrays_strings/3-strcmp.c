88fa2002c9f9c0bd978747100259fa3af6ff2d11
Andres Cortes <1805@holbertonschool.com>
2020-06-27 15:50:47 +0000

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,25 +7,34 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a;
-	int b;
-	int oxy;
 
-	for (a = 0; *(s1 + a) != '\0'; a++)
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 - *s2);
+}
+
+		/**
+		for (a = 0; *(s1 + a) != '\0'; a++)
 	{
 	}
+	a--;
 	for (b = 0; *(s2 + b) != '\0'; b++)
 	{
 	}
-
-	while (*(s1 + a) != '\0')
+	b--;
+	if (s1[a] == s2[b])
 	{
-		if (*(s2 + b) > *(s1 + a))
-			oxy = -15;
-		if (*(s2 + b) < *(s1 + a))
-			oxy = 15;
-		if (*(s1 + a) == '\0')
-			oxy = 0;
+		return (0);
 	}
-	return oxy;
+	else
+	{
+		return (s1[a] - s2[b]);
+	}
 }
+		*/
