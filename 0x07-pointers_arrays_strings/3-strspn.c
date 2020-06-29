af28f828bc3b257e15d240ed6d4f3669b0efae98
Andres Cortes <1805@holbertonschool.com>
2020-06-29 18:13:24 +0000

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -29,11 +29,11 @@ unsigned int _strspn(char *s, char *accept)
 	int zilo = 0, q, w;
 	int tom = prefix(s), ate = prefix(accept);
 
-	for (q = 0; q < tom; q++)
+	for (q = 0; q < ate; q++)
 	{
-		for (w = 0; w < ate; w++)
+		for (w = 0; w < tom; w++)
 		{
-			if (s[q] == accept[w])
+			if (accept[q] == s[w])
 			{
 				zilo++;
 			}
