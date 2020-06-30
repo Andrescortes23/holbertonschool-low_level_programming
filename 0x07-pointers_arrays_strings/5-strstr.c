1cdb97ce36ef8d63f339df630ba26318077f8482
Andres Cortes <1805@holbertonschool.com>
2020-06-30 18:46:17 +0000

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,36 +1,33 @@
 #include "holberton.h"
 /**
- *slad - to find char
- *@s: string to scan
- *@c: char to find
- *Return: s
- */
-char *slad(char *s, char c)
-{
-	while (*s)
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
-	return ('\0');
-}
-
-/**
- *_strstr - search for a byte
- *@haystack: string to scan
- *@needle: substring to match
- *Return: haystack
+ *
+ *
+ *
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
+	char *a = haystack;
+	char *b = needle;
+
+	while (*haystack != '\0' && *b != '\0')
 	{
-		if (slad(haystack, *needle))
+		if (*haystack == *b)
 		{
-			return (haystack);
+			haystack++;
+			b++;
 		}
-		haystack++;
+		else
+		{
+			haystack++;
+			a = haystack;
+			b = needle;
+		}
+
 	}
-	return (0);
+	if (*b == '\0')
+	{
+		return (a);
+	}
+	else
+		return (0);
 }
