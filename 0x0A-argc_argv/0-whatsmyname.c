17cab85fefdb288a1308c36e0baefffed65c5f61
Andres Cortes <1805@holbertonschool.com>
2020-07-05 06:33:52 +0000

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - program to print
+ *@argc: arguments counter
+ *@argv: string
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
