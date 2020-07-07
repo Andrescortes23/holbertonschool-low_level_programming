3b3280112725f6d45086121e0d450cf1c8462d64
Andres Cortes <1805@holbertonschool.com>
2020-07-07 22:19:39 +0000

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	omy = malloc(sizeof(char) * *str);
+	omy = malloc(sizeof(char) * *str + 1);
 	if (omy == NULL)
 		return (NULL);
 	for (uva = 0; uva < *str; uva++)
