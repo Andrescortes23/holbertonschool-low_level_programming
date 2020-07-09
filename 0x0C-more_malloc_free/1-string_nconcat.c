97052c3a3508f0a5fbb494a3270aaca11f91f115
Andres Cortes <1805@holbertonschool.com>
2020-07-09 23:28:33 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,19 @@
 #include <stdlib.h>
 /**
+ *
+ *
+ *
+ */
+int len(char *s)
+{
+	unsigned int index, counter;
+
+	for (index = 0; s[index] != '\0'; index++)
+		counter++;
+	return (counter);
+}
+
+/**
  *string_nconcat - concatenate 2 strings
  *@s1: string 1
  *@s2: string 2
@@ -8,31 +22,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *tylon;
-	unsigned int a, b, c, d;
+	char *s3;
+	unsigned int s2len, c, d;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-	}
-	if (b > n)
-		b = n;
+	s2len = len(s2);
+	if (s2len > n)
+		s2len = n;
 
-	tylon = malloc(sizeof(char) * (a + b + 1));
-	if (tylon == NULL)
+	s3 = malloc(sizeof(char) * (len(s1) + s2len + 1));
+	if (s3 == NULL)
 		return (NULL);
 
-	for (c = 0; s1[c] != '\0'; c++)
-		tylon[c] = s1[c];
-	for (; d < b; c++, d++)
-		tylon[c] = s2[d];
-	tylon[c] = '\0';
-	return (tylon);
+	for (c = 0; s1[c]; c++)
+		s3[c] = s1[c];
+	for (d = 0; d < s2len; c++, d++)
+		s3[c] = s2[d];
+	s3[c] = '\0';
+	return (s3);
 }
