601d42a5e77b265f7ebdd1def568cdd6a60de832
Andres Cortes <1805@holbertonschool.com>
2020-07-14 01:32:39 +0000

    dobi

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 int _putchar(char c);
+/**
+ *struct dog - struct with dog information
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
 struct dog
 {
 	char *name;
