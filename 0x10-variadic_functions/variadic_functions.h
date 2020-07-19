790bd71db45f41f2d565414aa780b0cbf1095aef
Andres Cortes <1805@holbertonschool.com>
2020-07-19 19:56:43 +0000

    add task 4 elements

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
+#include <stdarg.h>
+typedef struct datatype
+{
+	char x;
+	void (*func)(va_list param);
+} funcs;
+void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
