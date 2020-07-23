48ad753c05b6a44d7f50045837fcd67758ebac54
Andrescortes23 <andresccortes23@icloud.com>
2020-07-23 18:51:36 +0000

    crrecting code

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,8 +8,12 @@ size_t list_len(const list_t *h)
 {
 	unsigned int counter;
 
-	for (counter = 0; h[counter].str != '\0'; counter++)
+	if (h == NULL)
+		return (0);
+
+	for (counter = 0; h != NULL; counter++)
 	{
+		h = h->next;
 	}
 	return (counter);
 }
