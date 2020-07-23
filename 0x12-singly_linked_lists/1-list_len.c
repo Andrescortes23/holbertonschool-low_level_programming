35216392669b63640f44dbff3f3c278c2d9cbdf1
Andrescortes23 <andresccortes23@icloud.com>
2020-07-23 18:40:04 +0000

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int counter;
+
+	for (counter = 0; h[counter].str != '\0'; counter++)
+	{
+	}
+	return (counter);
+}
