7bd3a60136e9acfe4891f1666d237f753a592261
Andrescortes23 <andresccortes23@icloud.com>
2020-07-24 04:31:24 +0000

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <string.h>
+/**
+ *add_node_end - add node to the end
+ *@head: node adress
+ *@str: string to duplicate
+ *Return: New node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int index = 0, lenname;
+	list_t *node, *tmp;
+	char *name;
+
+	node = malloc(sizeof(list_t));
+	if (nodo == NULL)
+		return (NULL);
+
+	name = strdup(str);
+	while (*str)
+	{
+		str++;
+		index++;
+	}
+	lenname = index;
+
+	nodo->str = name;
+	nodo->len = lenname;
+	nodo->next = NULL;
+
+	if (*head == NULL)
+		*head = node;
+	else
+	{
+		while (tmp->next !=NULL)
