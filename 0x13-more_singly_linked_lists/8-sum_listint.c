ab6fd080ce408d5ed4b304b6fb008e2297c68e92
Andrescortes23 <andresccortes23@icloud.com>
2020-07-29 01:19:21 +0000

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *sum_listint - sum all data of nodes
+ *@head: pointer to frist node
+ *Return: sum of all data
+ */
+int sum_listint(listint_t *head)
+{
+	int suma;
+	listint_t *tmp = head;
+
+	if (tmp == NULL)
+		return (0);
+
+	while (tmp)
+	{
+		suma += tmp->n;
+		tmp = tmp->next;
+	}
+	return (suma);
+}
