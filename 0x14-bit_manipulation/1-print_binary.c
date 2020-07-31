d5d11630b35b8b3a3b02f878b312711c7465fbf2
Andrescortes23 <andresccortes23@icloud.com>
2020-07-31 02:19:04 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_binary - to convert int number in binary
+ *@n: int number
+ *Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
