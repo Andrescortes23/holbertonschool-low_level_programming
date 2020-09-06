281ce9622f3859c7c5e70f830d56efe512750a4f
Andrescortes23 <andresccortes23@icloud.com>
2020-09-06 23:53:52 +0000

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_dlistint - to free nodes
+ *@head: pointer to nodes
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	tmp = head;
+	while (head->next != NULL)
+	{
+		tmp = tmp->next;
+		free(head);
+		head = tmp;
+	}
+}
