704cffa254239163af5261d22c023e989345ca41
Andrescortes23 <andresccortes23@icloud.com>
2021-01-14 20:31:49 +0000

    Header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCHALOGS_H
+#define _SEARCHALGOS_H
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
